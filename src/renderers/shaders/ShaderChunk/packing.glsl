e159c3fc1c357b8d354b8a0a601f1d11dbb751ce
Ben Houston <ben@exocortex.com>
2018-02-22 09:46:21 -0500

    allow switching depth packing between base 255 and base 256.  Both have artifacts.

--- src/renderers/shaders/ShaderChunk/packing.glsl
+++ src/renderers/shaders/ShaderChunk/packing.glsl
@@ -5,7 +5,32 @@ vec3 packNormalToRGB( const in vec3 normal ) {
 vec3 unpackRGBToNormal( const in vec3 rgb ) {
   return 2.0 * rgb.xyz - 1.0;
 }
- 
+
+//#define USE_DEPTH_PACK_256
+
+#ifdef USE_DEPTH_PACK_256
+
+const float PackUpscale = 256. / 255.; // fraction -> 0..1 (including 1)	+vec4 packDepthToRGBA( in highp float v ) {
+const float UnpackDownscale = 255. / 256.; // 0..1 -> fraction (excluding 1)	
+ 	 
+const vec3 PackFactors = vec3( 256. * 256. * 256., 256. * 256.,  256. );
+const vec4 UnpackFactors = UnpackDownscale / vec4( PackFactors, 1. );
+ 	 
+const float ShiftRight8 = 1. / 256.;
+ 	 
+vec4 packDepthToRGBA( const in float v ) {	
+	vec4 r = vec4( fract( v * PackFactors ), v );	
+	r.yzw -= r.xyz * ShiftRight8; // tidy overflow	
+	return r * PackUpscale;	
+}
+ 	 
+float unpackRGBAToDepth( const in vec4 v ) {
+	return dot( v, UnpackFactors );
+}
+
+#else
+
+
 #define DEPTH_PACK_BYTES 4
 
 vec4 packDepthToRGBA( in highp float v ) {
@@ -36,6 +61,8 @@ float unpackRGBAToDepth( const in highp vec4 v ) {
   return depth;
 
 }
+
+#endif
 
 // NOTE: viewZ/eyeZ is < 0 when in front of the camera per OpenGL conventions
 

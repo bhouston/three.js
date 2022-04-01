6dab11c27783cbe35f3e28aa58601b3430b9d599
Ben Houston <bhouston@threekit.com>
2022-04-01 16:53:34 -0400

    added debug mode to TRAA Render Pass

--- examples/jsm/postprocessing/TRAARenderPass.js
+++ examples/jsm/postprocessing/TRAARenderPass.js
@@ -7,66 +7,64 @@ import {
 	RGBAFormat,
 	ShaderMaterial,
 	UniformsUtils,
-	WebGLRenderTarget
-} from 'three';
+	WebGLRenderTarget,
+} from "three";
 
-import { Pass, FullScreenQuad } from './Pass.js';
-import { CopyShader } from '../shaders/CopyShader.js';
-import { TRAAShader } from '../shaders/TRAAShader.js';
-import { VelocityShader, updateMatrixWorldPreview } from '../shaders/VelocityShader.js';
+import { Pass, FullScreenQuad } from "./Pass.js";
+import { CopyShader } from "../shaders/CopyShader.js";
+import { TRAAShader } from "../shaders/TRAAShader.js";
+import {
+	VelocityShader,
+	updateMatrixWorldPreview,
+} from "../shaders/VelocityShader.js";
 
 class TRAARenderPass extends Pass {
-
-	constructor( scene, camera, resolution ) {
-
+	constructor(scene, camera, resolution) {
 		super();
 
 		this.scene = scene;
 		this.camera = camera;
+		this.output = TRAARenderPass.OUTPUT.Beauty;
 
 		//this.orthoScene = new Scene();
 		//this.orthoCamera = new OrthographicCamera(-1, 1, 1, -1, -0.01, 1000);
 
-		this.fsQuad = new FullScreenQuad( null );
+		this.fsQuad = new FullScreenQuad(null);
 
-		this.traaMaterial = new ShaderMaterial( {
-			uniforms: UniformsUtils.clone( TRAAShader.uniforms ),
+		this.traaMaterial = new ShaderMaterial({
+			uniforms: UniformsUtils.clone(TRAAShader.uniforms),
 			vertexShader: TRAAShader.vertexShader,
 			fragmentShader: TRAAShader.fragmentShader,
 			defines: TRAAShader.defines,
 			side: DoubleSide,
-		} );
+		});
 
-		this.velocityMaterial = new ShaderMaterial( {
-			uniforms: UniformsUtils.clone( VelocityShader.uniforms ),
+		this.velocityMaterial = new ShaderMaterial({
+			uniforms: UniformsUtils.clone(VelocityShader.uniforms),
 			vertexShader: VelocityShader.vertexShader,
 			fragmentShader: VelocityShader.fragmentShader,
 			side: DoubleSide,
-		} );
+		});
 		this.velocityMaterial.extensions.derivatives = true;
 
 		this.oldClearColor = new Color();
 		this.oldClearAlpha = 1;
 		this.needsSwap = false;
 
-		this.copyMaterial = new ShaderMaterial( {
-			uniforms: UniformsUtils.clone( CopyShader.uniforms ),
+		this.copyMaterial = new ShaderMaterial({
+			uniforms: UniformsUtils.clone(CopyShader.uniforms),
 			vertexShader: CopyShader.vertexShader,
 			fragmentShader: CopyShader.fragmentShader,
 			transparent: true,
 			depthWrite: false,
-		} );
+		});
 
-		this.accumulatedBeautyRenderTarget = new WebGLRenderTarget(
-			256,
-			256,
-			{
-				minFilter: LinearFilter,
-				magFilter: LinearFilter,
-				format: RGBAFormat,
-				stencilBuffer: true,
-			}
-		);
+		this.previousBeautyRenderTarget = new WebGLRenderTarget(256, 256, {
+			minFilter: LinearFilter,
+			magFilter: LinearFilter,
+			format: RGBAFormat,
+			stencilBuffer: true,
+		});
 
 		this.previousProjectionViewMatrix = new Matrix4();
 		this.currentProjectionViewMatrix = new Matrix4();
@@ -77,24 +75,22 @@ class TRAARenderPass extends Pass {
 		this.staticMode = false;
 
 		this.depthTexture = null;
-
 	}
 
 	dispose() {
-
-		if ( this.accumulatedBeautyRenderTarget ) this.accumulatedBeautyRenderTarget.dispose();
-
+		if (this.previousBeautyRenderTarget)
+			this.previousBeautyRenderTarget.dispose();
 	}
 
-	setSize( width, height ) {
+	setSize(width, height) {
+		if (this.previousBeautyRenderTarget)
+			this.previousBeautyRenderTarget.setSize(width, height);
+		if (this.velocityRenderTarget)
+			this.velocityRenderTarget.setSize(width, height);
 
-		if ( this.accumulatedBeautyRenderTarget ) this.accumulatedBeautyRenderTarget.setSize( width, height );
-		if ( this.velocityRenderTarget ) this.velocityRenderTarget.setSize( width, height );
-
-		this.projectionMatrix.copy( this.camera.projectionMatrix );
+		this.projectionMatrix.copy(this.camera.projectionMatrix);
 
 		this.resetPending = true;
-
 	}
 
 	renderOverride(
@@ -104,7 +100,6 @@ class TRAARenderPass extends Pass {
 		clearColor,
 		clearAlpha
 	) {
-
 		var originalClearColor = renderer.getClearColor().getHex();
 		var originalClearAlpha = renderer.getClearAlpha();
 		var originalAutoClear = renderer.autoClear;
@@ -115,11 +110,9 @@ class TRAARenderPass extends Pass {
 		clearAlpha = overrideMaterial.clearAlpha || clearAlpha;
 		var clearNeeded = clearColor !== undefined && clearColor !== null;
 
-		if ( clearNeeded ) {
-
-			renderer.setClearColor( clearColor );
-			renderer.setClearAlpha( clearAlpha || 0.0 );
-
+		if (clearNeeded) {
+			renderer.setClearColor(clearColor);
+			renderer.setClearAlpha(clearAlpha || 0.0);
 		}
 
 		this.scene.overrideMaterial = overrideMaterial;
@@ -136,33 +129,29 @@ class TRAARenderPass extends Pass {
 
 		// restore original state
 		renderer.autoClear = originalAutoClear;
-		renderer.setClearColor( originalClearColor );
-		renderer.setClearAlpha( originalClearAlpha );
+		renderer.setClearColor(originalClearColor);
+		renderer.setClearAlpha(originalClearAlpha);
 	}
 
-	render( renderer, writeBuffer, readBuffer, delta, maskActive, overrideCamera ) {
+	render(renderer, writeBuffer, readBuffer, delta, maskActive, overrideCamera) {
+		const camera = overrideCamera || this.camera;
 
-		var camera = overrideCamera || this.camera;
-
-
-		renderer.getClearColor( this.oldClearColor );
+		renderer.getClearColor(this.oldClearColor);
 		this.oldClearAlpha = renderer.getClearAlpha();
-		var oldAutoClear = renderer.autoClear;
-		var oldAutoClearDepth = renderer.autoClearDepth;
+		const oldAutoClear = renderer.autoClear;
+		const oldAutoClearDepth = renderer.autoClearDepth;
+		const oldAutoClearColor = renderer.autoClearColor;
 
-		var width = writeBuffer.width,
-			height = writeBuffer.height;
+		const width = writeBuffer.width;
+		const height = writeBuffer.height;
 
 		if ( ! this.velocityRenderTarget ) {
 
-			var params = {
+			this.velocityRenderTarget = new WebGLRenderTarget( width, height, {
 				minFilter: LinearFilter,
 				magFilter: LinearFilter,
 				format: RGBAFormat,
-			};
-
-			this.velocityRenderTarget = new WebGLRenderTarget( width, height, params );
-
+			} );
 		}
 
 		this.currentProjectionViewMatrix.multiplyMatrices(
@@ -170,26 +159,23 @@ class TRAARenderPass extends Pass {
 			camera.matrixWorldInverse
 		);
 
-		renderer.getClearColor( this.oldClearColor );
-		this.oldClearAlpha = renderer.getClearAlpha();
-		var oldAutoClear = renderer.autoClear;
-		var oldAutoClearDepth = renderer.autoClearDepth;
-		var oldAutoClearColor = renderer.autoClearColor;
+		//
+		// Step 1: Render Velocity to the Velocity Buffer
+		//
 
-		renderer.autoClear = false;
-
-		renderer.setClearColor( new Color( 0, 0, 0 ) );
-		renderer.setClearAlpha( 0 );
-
-		this.velocityMaterial.uniforms.currentProjectionViewMatrix.value.copy(
+		const velocityUniforms = this.velocityMaterial.uniforms;
+		velocityUniforms.currentProjectionViewMatrix.value.copy(
 			this.currentProjectionViewMatrix
 		);
-		this.velocityMaterial.uniforms.previousProjectionViewMatrix.value.copy(
+		velocityUniforms.previousProjectionViewMatrix.value.copy(
 			this.previousProjectionViewMatrix
 		);
 
-		//renderer.autoClearColor = true;
 		this.scene.overrideMaterial = this.velocityMaterial;
+
+		renderer.autoClear = false;
+		renderer.setClearColor(new Color(0, 0, 0));
+		renderer.setClearAlpha(0);
 		renderer.render(
 			this.scene,
 			camera,
@@ -197,8 +183,14 @@ class TRAARenderPass extends Pass {
 			true,
 			this.visibilityFunc
 		);
+
 		this.scene.overrideMaterial = null;
-		updateMatrixWorldPreview( this.scene );
+
+		updateMatrixWorldPreview(this.scene);
+
+		//
+		// Step 2: Render the TRAA Resolver
+		//
 
 		const traaUniforms = this.traaMaterial.uniforms;
 
@@ -212,7 +204,7 @@ class TRAARenderPass extends Pass {
 		}
 
 		traaUniforms.currentBeauty.value = readBuffer.texture;
-		traaUniforms.previousBeauty.value = this.accumulatedBeautyRenderTarget.texture;
+		traaUniforms.previousBeauty.value = this.previousBeautyRenderTarget.texture;
 		traaUniforms.DEPTH_PACKING = this.depthTexture.depthPacking; // Enum to 0/1 value?  Is this a bug?
 		traaUniforms.tDepth.value = this.depthTexture;
 		traaUniforms.tVelocity.value = this.velocityRenderTarget.texture;
@@ -232,39 +224,70 @@ class TRAARenderPass extends Pass {
 
 		}
 
-		traaUniforms.cameraInverseProjectionMatrix.value.copy( this.projectionMatrix ).invert();
+		traaUniforms.cameraInverseProjectionMatrix.value
+			.copy(this.projectionMatrix)
+			.invert();
 		traaUniforms.cameraProjectionMatrix.value.copy( this.projectionMatrix );
 		traaUniforms.cameraInverseViewMatrix.value.copy( camera.matrixWorld );
 		traaUniforms.cameraNearFar.value.set( camera.near, camera.far );
 		traaUniforms.textureSize.value.set( width, height );
 		traaUniforms.minSampleWeight.value = 1.0 / this.numSamplesPerAccumulation;
 
-		//renderer.autoClearColor = true;
-		//renderer.autoClearDepth = false;
 		renderer.autoClearDepth = false;
-		renderer.setRenderTarget( null );
+		renderer.setRenderTarget( writeBuffer );
 		this.fsQuad.material = this.traaMaterial;
 		this.fsQuad.render( renderer );
+
+		//
+		// Step 3: Remember this pass so we can reuse it on the next frame
+		//
 
 		this.copyMaterial.uniforms.tDiffuse.value = writeBuffer.texture;
 		this.copyMaterial.uniforms.opacity.value = 1;
 
 		this.fsQuad.material = this.copyMaterial;
-		renderer.setRenderTarget( this.accumulatedBeautyRenderTarget );
+		renderer.setRenderTarget( this.previousBeautyRenderTarget );
 		this.fsQuad.render( renderer );
 
 		renderer.setRenderTarget( readBuffer );
 		this.fsQuad.render( renderer );
+
+		switch ( this.output ) {
+
+			case TRAARenderPass.OUTPUT.Beauty:
+				break;
+
+			case TRAARenderPass.OUTPUT.Raw:
+				this.copyMaterial.uniforms.tDiffuse.value = readBuffer.texture;
+				renderer.setRenderTarget( writeBuffer );
+				this.fsQuad.render( renderer );
+				break;
+
+			case TRAARenderPass.OUTPUT.Velocity:
+				this.copyMaterial.uniforms.tDiffuse.value = this.velocityRenderTarget.texture;
+				renderer.setRenderTarget( writeBuffer );
+				this.fsQuad.render( renderer );
+				break;
+
+		}
 
 		renderer.setClearColor( this.oldClearColor );
 		renderer.setClearAlpha( this.oldClearAlpha );
 		renderer.autoClear = oldAutoClear;
 		renderer.autoClearColor = oldAutoClearColor;
 		renderer.autoClearDepth = oldAutoClearDepth;
+
 		this.previousProjectionViewMatrix.copy( this.currentProjectionViewMatrix );
 
 	}
 
 }
+
+TRAARenderPass.OUTPUT = {
+	Beauty: 1,
+	Raw: 0,
+	Velocity: 2,
+	Depth: 3,
+};
 
 export { TRAARenderPass };
